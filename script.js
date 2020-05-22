3a912d4f92c557309e43c296cefbf4d620e5dbf6
Marci Feiden <mfeiden@gmail.com>
2020-05-22 20:16:59 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+//test function
+$(document).ready(function() {
+  $.ajax({
+    type: 'GET',
+    url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=1',
+    dataType: 'json',
+    success: function (response, textStatus) {
+      console.log(response);
+    },
+    error: function (request, textStatus, errorMessage) {
+      console.log(errorMessage);
+    }
+  })
+});
