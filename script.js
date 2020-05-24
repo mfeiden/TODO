c8de6ceebb9cf4abef2ab862e5e2b17b1e6df064
Marci Feiden <mfeiden@gmail.com>
2020-05-24 00:26:24 +0000

    Added script for UPDATE function

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+var updateList = function() {
+  $.ajax({
+    type: 'GET',
+    url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=160',
+    dataType: 'json',
+    success: function (response, textStatus) {
+      $('#list').empty();
+      response.tasks.forEach(function (task) {
+        $('#list').append('<tr><td><label class="check"><input type="checkbox" class="mr-2"><span>' + task.content + '</span></label></td><td><button class="remove bg-white" style="border:none"><i class="far fa-trash-alt fa-sm"></i></button></td></tr>');
+      })
+      console.log(response)
+    },
+    error: function (request, textStatus, errorMessage) {
+      console.log(errorMessage);
+    }
+  });
+}
 
 var addTask = function() {
   $.ajax({
@@ -11,38 +28,41 @@ var addTask = function() {
       }
     }),
     success: function (response, textStatus) {
-      var newTask = response.task.content;
-       $('#list').append('<tr><td><label class="check"><input type="checkbox" class="mr-2"><span>' + newTask + '</span></label></td><td><button class="remove bg-white" style="border:none"><i class="far fa-trash-alt fa-sm"></i></button></td></tr>');
-       $('#new').val('');
+      updateList();
+      $('#new').val('');
+    },
+    error: function (request, textStatus, errorMessage) {
+      console.log(errorMessage);
+    }
+  });
+}
+
+var removeTask = function(id) {
+  $.ajax({
+    type: 'DELETE',
+    url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=160',
+    success: function (response, textStatus) {
+      console.log(response);   
     },
     error: function (request, textStatus, errorMessage) {
       console.log(errorMessage);
     }
   
   })
+
 }
 
 
 
 
-
 $(document).ready(function() {
+  updateList();
+  
   $('#new').on('keyup', function(event) {
+    event.preventDefault();
     if (event.key === 'Enter') {
       addTask();
     }
-  
-  
-   $.ajax({
-        type: 'GET',
-        url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=160',
-        dataType: 'json',
-        success: function (response, textStatus) {
-          console.log(response);
-        },
-        error: function (request, textStatus, errorMessage) {
-          console.log(errorMessage);
-        }
-      })
   })
+
 });
